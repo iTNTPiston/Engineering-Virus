082ad230a6ca2b36b0a000f2f4e755cc4ffce61c
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-11-06 16:32:51 -0500

    Created Util classes

---
+++ src/main/java/com/tntp/ev/util/DebugUtil.java
@@ -0,0 +1,9 @@
+package com.tntp.ev.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DebugUtil {
+  public static final int VERSION = 1;
+  public static Logger log = LogManager.getLogger("EV-Log");
+}
---
+++ src/main/java/com/tntp/ev/util/UniversalUtil.java
@@ -0,0 +1,5 @@
+package com.tntp.ev.util;
+
+public class UniversalUtil {
+  public static final int VERSION = 1;
+}
