f642c9cbde13d66da10fdbd6a85195718764078b
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-11-06 21:51:31 -0500

    Add registration to proxy

--- src/main/java/com/tntp/ev/core/Proxy.java
+++ src/main/java/com/tntp/ev/core/Proxy.java
@@ -1,12 +1,16 @@
 package com.tntp.ev.core;
 
+import com.tntp.ev.init.EVBlocks;
+import com.tntp.ev.init.EVItems;
+
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class Proxy {
   public void preInit(FMLPreInitializationEvent event) {
-
+    EVBlocks.loadBlocks();
+    EVItems.loadItems();
   }
 
   public void init(FMLInitializationEvent event) {
