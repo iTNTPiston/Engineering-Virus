d3b9978ffa5ed3aa2cf278862f38bb3bc8a3e5f0
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-11-08 16:52:21 -0500

    Name changes

--- src/main/java/com/tntp/ev/tileentity/STileEV.java
+++ src/main/java/com/tntp/ev/tileentity/STileEV.java
@@ -2,6 +2,12 @@ package com.tntp.ev.tileentity;
 
 import net.minecraft.tileentity.TileEntity;
 
-public class STileSuper extends TileEntity {
+/**
+ * Super class of (hopefully) all tileentities in EV
+ * 
+ * @author iTNTPiston
+ *
+ */
+public class STileEV extends TileEntity {
 
 }
