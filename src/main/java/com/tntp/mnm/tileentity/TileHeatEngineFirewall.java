5c8d522b69aa7b9427181f71edb496bf22e188fd
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-11-14 12:31:31 -0500

    Created HeatEngine TileEntity

---
+++ src/main/java/com/tntp/mnm/tileentity/TileHeatEngineFirewall.java
@@ -0,0 +1,53 @@
+package com.tntp.mnm.tileentity;
+
+import com.tntp.mnm.api.ek.IHeatSink;
+import com.tntp.mnm.init.MNMBlocks;
+import com.tntp.mnm.util.DirUtil;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class TileHeatEngineFirewall extends STileHeatNode implements IHeatSink {
+  private static int MAX_RATE = 2, MAX_EK = 1000, MAX_BOOSTED = 3, EK_TO_MZ = 10;
+  private boolean boosted;
+
+  public TileHeatEngineFirewall() {
+    setEK(0);
+    setMaxEK(MAX_EK);
+    boosted = false;
+  }
+
+  @Override
+  public boolean isSinkSide(int side) {
+    return side == DirUtil.D_D_MY;
+  }
+
+  @Override
+  public void rescan() {
+    super.rescan();
+
+    int structure = 0;
+    for (int xx = xCoord - 1; xx <= xCoord + 1; xx++) {
+      for (int zz = zCoord - 1; zz <= zCoord + 1; zz++) {
+        if (worldObj.getBlock(xx, yCoord, zz) == MNMBlocks.blockFirewall) {
+          structure++;
+        }
+      }
+    }
+    boosted = structure == 8;
+
+  }
+
+  @Override
+  public void writeToNBT(NBTTagCompound tag) {
+    super.writeToNBT(tag);
+    tag.setBoolean("boosted", boosted);
+  }
+
+  @Override
+  public void readFromNBT(NBTTagCompound tag) {
+    super.readFromNBT(tag);
+    boosted = tag.getBoolean("boosted");
+  }
+
+}
