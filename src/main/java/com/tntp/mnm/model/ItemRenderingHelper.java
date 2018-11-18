0a20e3e483701bfec2eb7439ecb78f553ac3c13e
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-11-17 16:36:24 -0800

    Add Third Person Item Model

--- src/main/java/com/tntp/mnm/model/ItemRenderingHelper.java
+++ src/main/java/com/tntp/mnm/model/ItemRenderingHelper.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -58,6 +59,9 @@ public class ItemRenderingHelper implements IItemRenderer {
       break;
     case ENTITY:
       renderEntityItem(item, (RenderBlocks) data[0], (EntityItem) data[1]);
+      break;
+    case EQUIPPED:
+      renderThirdPersonItem(item, (RenderBlocks) data[0], (EntityLivingBase) data[1]);
     }
   }
 
@@ -79,9 +83,15 @@ public class ItemRenderingHelper implements IItemRenderer {
     GL11.glPopMatrix();
   }
 
-  public void renderThirdPersonItem(ItemStack stack, RenderBlocks render, EntityLiving entity) {
+  public void renderThirdPersonItem(ItemStack stack, RenderBlocks render, EntityLivingBase entity) {
     WaveObjRenderer obj = getRenderer(stack);
+    GL11.glPushMatrix();
+    GL11.glTranslatef(1f, 1 / 2f, 1f);
+    GL11.glRotatef(45, 0, 1, 0);
+    GL11.glRotatef(105, 1, 0, 0);
+    GL11.glTranslatef(0, -0.7f, 0);
     obj.render();
+    GL11.glPopMatrix();
   }
 
 }
