453e6c548d8a6c7fcee2e7978cac19a71712ce76
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-12-08 15:42:54 -0500

    [MOD]Small Hammer on heat pipes

--- src/main/java/com/tntp/mnm/item/tools/ItemSmallHammer.java
+++ src/main/java/com/tntp/mnm/item/tools/ItemSmallHammer.java
@@ -1,12 +1,33 @@
 package com.tntp.mnm.item.tools;
 
 import com.tntp.mnm.gui.GuiTabType;
+import com.tntp.mnm.init.MNMBlocks;
 import com.tntp.mnm.item.SItemTool;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemSmallHammer extends SItemTool {
 
   public ItemSmallHammer() {
     super("smallHammer");
+  }
+
+  @Override
+  public boolean onToolUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX,
+      float hitY, float hitZ) {
+    Block b = world.getBlock(x, y, z);
+    if (b == MNMBlocks.heatPipe) {
+      if (!world.isRemote) {
+        int meta = world.getBlockMetadata(x, y, z);
+        meta = (meta + 1) & 15;
+        world.setBlockMetadataWithNotify(x, y, z, meta, 3);
+      }
+      return true;
+    }
+    return false;
   }
 
 }
