78879afcecf8eaa6e7529a4e71f114fd44b9954f
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2018-12-20 21:49:23 -0500

    [ADD]Structure Module for looking at mainframe's structure

--- src/main/java/com/tntp/mnm/tileentity/TileStructureModule.java
+++ src/main/java/com/tntp/mnm/tileentity/TileStructureModule.java
@@ -1,10 +1,40 @@
 package com.tntp.mnm.tileentity;
 
-public class TileStructureModule extends STilePOB {
+import java.util.List;
+
+import com.tntp.mnm.gui.cont.ITileDataCont;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
+
+public class TileStructureModule extends STilePOB implements ITileDataCont {
 
   public TileStructureModule(int size) {
     super(size);
     // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String getContainerGui() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void addContainerSlots(List<Slot> slots) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean canReadData() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean isUseableByPlayer(EntityPlayer player) {
+    return super.isUseableByPlayer(player) && canReadData();
   }
 
 }
